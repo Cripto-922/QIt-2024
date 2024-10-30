f26c65a876d9dbfdd215fee15188972da58d9c81
aidynserikbolat <aydingaming554@gmail.com>
2024-10-30 10:06:48 +0500

    example 1

---
+++ example's/js.js
@@ -0,0 +1,14 @@
+var name = "nazar"; //ainymaly
+
+name = "abumansur";
+
+name = "cripto";
+
+name = "langadaaszz";
+const phone = "123.456.789.12";
+
+phone = "987.765.654.12";
+
+console.log(phone):// khate kaitarady
+
+dacument.getElementById("username").innerHtml = name;
