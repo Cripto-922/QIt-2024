d11e6d3020b7afb78a4581904798cebe2027c139
aidynserikbolat <aydingaming554@gmail.com>
2024-10-30 15:48:22 +0500

    30.10 HomeWork

---
+++ home.work's/30.10/script.js
@@ -0,0 +1,11 @@
+var ModelName = "Matiz";
+var Color = "green";
+var WheelsCount = 4;
+var isOffRoad = "no"
+var rating = "10/5"
+
+console.log("koliktin markasi", ModelName);
+console.log("tusi", Color);
+console.log("dongelekter", WheelsCount);
+console.log("kez kelgen jolga juruge arnalmagan", isOffRoad);
+console.log("dengeyi", rating);
