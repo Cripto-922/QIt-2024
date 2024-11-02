b83418efd2ef84a56a74389ac00198627efe59a0
aidynserikbolat <aydingaming554@gmail.com>
2024-11-02 14:42:13 +0500

    31.10 hm

---
+++ example's/31.10.js
@@ -0,0 +1,132 @@
+// var x = 3;
+// var y = 8;
+
+// var sum = x-y; // minus
+
+// sum // natije-result
+
+// console.log(sum); //11
+
+// var a = 4;
+// var b = 20;
+
+// console.log(a * b); // kobeitu , 80
+
+// let z = 10;
+// let r = 5;
+
+// console.log(z / r); //bolu , 2
+
+// var num1 = 20;
+// var num2 = 3;
+
+// console.log(20 % 3); //kaldyg , 2
+
+// let num1 = 25;
+
+// if (num1 % 2 == 0) {
+//     console.log("bul san jup");
+// }
+
+// if (num1 % 2 !== 0) {
+//     console.log("bul san taq");
+// }
+
+// let num = 10;
+
+// num++;// increment
+
+// console.log(num); //11
+
+// let num2 = 9;
+
+// num2--; // decrement
+
+// console.log(num2);
+
+// let x = 20;
+
+// x += 5; //x = x + 5
+
+// console.log(x); //25 k
+
+// let y = 40;
+
+// y -= 7; // y = y - 7
+
+// console.log(y); //33
+
+// let c = 10;
+
+// c /= 2; //x = x /2
+
+// console.log(c); // bolu menshikteu 5
+
+// let b = 10;
+
+// b %= 3; //b =b % 3
+
+// console.log(b);//1 kaldyg menshk
+
+// var q = 20;
+
+// var w = 6;
+
+// //26 6.5 13.5
+// let result = (w + q) / 4+7; //ornek
+
+// console.log(result); //13.5
+
+// var a = 8;
+
+// var b = 5;
+
+// let p = (a+b) * 2;
+
+// let area = a * b;
+
+// let m = 20;
+
+// const g = 9.8
+
+// let force = m * g;
+
+// console.log("kushi: ", force);
+// console.log("perimetr: ", p);
+// console.log("audany: ", area);
+
+
+
+// var a = 20;
+// var b = 11;
+// var c = 3;
+// var d = 6;
+// var e = 5;
+
+// let total = (a + b) * c - d / e;
+//  console.log(total);
+
+var a = 10;
+var b = 15;
+var c = 23;
+
+// let result = a + b * c / a - b;
+
+// console.log(result);
+
+// var x = 11;
+// var y = 4457;
+// var z = 24;
+
+// let groupedValue = (x + y) * z;
+
+//  console.log(groupedValue);
+
+
+// let res = ((a * b) / c) / (z - 3);
+
+// console.log(res % 2 );
+
+let result = ((a / b) * 45) / (c - b - a) + 56;
+
+console.log(result);
