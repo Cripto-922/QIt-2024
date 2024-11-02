1e726391fec5931daf9d5820815160623c860d47
aidynserikbolat <aydingaming554@gmail.com>
2024-11-02 14:38:34 +0500

    31.10 HM

---
+++ example's/test.js
@@ -0,0 +1,12 @@
+// var streetName = "Maulenov, 92"; // String
+// var currentDay = "30" //Number
+// var AmirKeldi = false; //Boolen true = Shindyg, False-otyryk
+// var JanaSumka = null; //Bos man
+// var porshe = undefined; //jog ainymali
+
+
+// console.log("koshe", streetName);
+// console.log("kazyrgy kun", currentDay);
+// console.log("amir keldi", AmirKeldi);
+// console.log("jana satyp algan sumkanyn ishindegy zattar:", JanaSumka);
+// console.log("porshe bar", porshe);
