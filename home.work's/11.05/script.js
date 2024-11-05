d8a91f07774d5bdb9d67fbcf0d610ccdabbed709
aidynserikbolat <aydingaming554@gmail.com>
2024-11-05 17:55:17 +0500

    11.05 HW

---
+++ home.work's/11.05/script.js
@@ -0,0 +1,18 @@
+     // 1nsh tapsyrma
+let num = prompt("Jasynyzdy engizinyz!")
+
+if (num >= 18) {
+    alert("siz ereseksyz")
+} else {
+    alert("syz eresek emessyz")
+}
+
+     // 2nsh tapsyrma
+
+let temperature = prompt("bugng aua raydi jazynyz");
+
+if (temperature <= 25) {
+    alert("sizge kurtka jempyr kiip shgunyzga usinyz bildremin")
+} else {
+    alert("sizge shortk maik kiip shgunyzga usinis bildiremyn")
+}
