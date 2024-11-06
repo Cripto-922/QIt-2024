34b46d5df42d20b3abad529b8d032ee6cc93f855
aidynserikbolat <aydingaming554@gmail.com>
2024-11-06 21:27:58 +0500

    11.06 HM

---
+++ home.work's/11.06/script.js
@@ -0,0 +1,21 @@
+let Name = prompt ("atynyzdy jazynyz:");
+alert("kosh keldynyz syz sayahatka shykkali jatyrsyz. Syzdyn tandaynyz-syzdyn tagdyrynyzdy sheshedy");
+
+let tandau1 = prompt("syz kyiliska tap bolasyz. Solga barasyzba alde onga?" + "\n" + "sol | on");
+if (tandau1 == "sol") {
+    alert("syz sol jak joldy tandadynyz jane jolda tastardy koresyz.");
+}
+
+let tandau2 = prompt ("syz ozendy koryp tursyz. saldyn ustynen juzyp otesuzbe nemese kaik jasaysizba" + "\n" + "juzu | kaik");
+
+if (tandau2 == "juzu") {
+    alert("syz ozennen juzip ottiniz brag agys sizdi tomenge tartyp aketty"
+    )
+}
+let tanday3 =prompt("agystan kutlu ushin agashka jarmasasyzba nemese agynmen byrge juzesizbe" + "\n" + "jarmasu | juzu");
+if (tanday3 == "jarmasu") {
+    alert("syz aman kaldynyz jane ozennyn argy betyne ottiniz kuttyktaymin!")
+} else {
+    alert("agys sizdi alip ketty. okinishke oray sayahat satsyz aiaktaldi")
+}
+
