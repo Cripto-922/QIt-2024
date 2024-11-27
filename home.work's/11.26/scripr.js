7a288edd8ce06e264e929316abb06e946eed265d
aidynserikbolat <aydingaming554@gmail.com>
2024-11-27 08:42:27 +0500

    11.26 hw

---
+++ home.work's/11.26/scripr.js
@@ -0,0 +1,11 @@
+function nums() {
+    let nums = [];
+
+    while(true) {
+        let num = prompt("enter number");
+        if (num % 2 != 0) break;
+    }
+    return nums
+}
+
+console.log(nums());
