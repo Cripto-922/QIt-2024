896faadbf018fd7d6345c75b2dc51ddf5436798f
aidynserikbolat <aydingaming554@gmail.com>
2024-12-10 21:46:04 +0500

    12.10hw

---
+++ home.work's/12.10hw DOM/12.10.js
@@ -0,0 +1,14 @@
+const tittle = document.getElementById ("news-title");
+tittle.textContent = "dom alemine qosh keldynyz";
+const content = document.getElementById ("new-word1");
+content.textContent = "yes i know";
+const content2 = document.getElementById ("new-word2");
+content2.textContent = " DOM is my first step to web magic ^_^";
+const content3 = document.getElementById ("new-word3");
+content3.textContent = "AAAAAAAAAAAAAAAAAAAAAA";
+const button = document.getElementById ("button");
+button.textContent = "MY name is ONF1REEE";
+
+
+tittle.style.color = "red"
+content3.style.color = "green"
